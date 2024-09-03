00659b38760e48c8d813d4b446ec22b11c0c8e1e
Andrew Parahin <parahin6738@gmail.com>
2024-09-03 21:27:02 +0300

    Initial commit

---
+++ src/main/kotlin/com/example/plugins/Security.kt
@@ -0,0 +1,50 @@
+package com.example.plugins
+
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import com.auth0.jwt.interfaces.DecodedJWT
+import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
+import java.util.*
+
+fun Application.configureSecurity() {
+    authentication {
+        jwt("auth-jwt") {
+            realm = "ktor sample app"
+            verifier(
+                JWT
+                    .require(JwtConfig.algorithm)
+                    .withAudience(JwtConfig.audience)
+                    .withIssuer(JwtConfig.issuer)
+                    .build()
+            )
+            validate { credential ->
+                if (credential.payload.audience.contains(JwtConfig.audience)) JWTPrincipal(credential.payload) else null
+            }
+        }
+    }
+}
+
+object JwtConfig {
+    private const val secret = "your-secret-key"
+    internal const val issuer = "your-issuer"
+    internal const val audience = "your-audience"
+    private const val validityInMs = 36_000_00 * 10 // 10 hours
+
+    val algorithm = Algorithm.HMAC256(secret)
+
+    fun makeToken(email: String): String = JWT.create()
+        .withSubject("Authentication")
+        .withIssuer(issuer)
+        .withAudience(audience)
+        .withClaim("email", email)
+        .sign(algorithm)
+}
+
+fun isTokenValid(payload: DecodedJWT): Boolean {
+    val expiresAt = payload.expiresAt
+    return expiresAt != null && expiresAt.after(Date()) && payload.audience.contains(JwtConfig.audience)
+}
+
+
---
+++ src/main/kotlin/com/example/plugins/UsersSchema.kt
@@ -0,0 +1,61 @@
+package com.example.plugins
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.serialization.Serializable
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
+import org.jetbrains.exposed.sql.transactions.transaction
+
+@Serializable
+data class ExposedUser(val email: String, val password: String)
+
+class UserService(database: Database) {
+    object Users : Table() {
+        val id = integer("id").autoIncrement()
+        val email = varchar("email", length = 128)
+        val password = varchar("password", length = 128)
+
+        override val primaryKey = PrimaryKey(id)
+    }
+
+    init {
+        transaction(database) {
+            SchemaUtils.create(Users)
+        }
+    }
+
+    suspend fun create(user: ExposedUser): Int = dbQuery {
+        Users.insert {
+            it[email] = user.email
+            it[password] = user.password
+        }[Users.id]
+    }
+
+    suspend fun read(id: Int): ExposedUser? {
+        return dbQuery {
+            Users.select { Users.id eq id }
+                .map { ExposedUser(it[Users.email], it[Users.password]) }
+                .singleOrNull()
+        }
+    }
+
+    suspend fun update(id: Int, user: ExposedUser) {
+        dbQuery {
+            Users.update({ Users.id eq id }) {
+                it[email] = user.email
+                it[password] = user.password
+            }
+        }
+    }
+
+    suspend fun delete(id: Int) {
+        dbQuery {
+            Users.deleteWhere { Users.id.eq(id) }
+        }
+    }
+
+    private suspend fun <T> dbQuery(block: suspend () -> T): T =
+        newSuspendedTransaction(Dispatchers.IO) { block() }
+}
+
